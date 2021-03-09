cd4eb1fd0670e00fc069488c9611bd26bfa270ce
Mingjie Jiang <jiang@mingjie.dev>
2021-03-09 12:13:20 -0500

    Add comments to runner

--- runner.js
+++ runner.js
@@ -1,3 +1,11 @@
+/**
+ * Don't worry too much about what this file does! \
+ * 
+ * This is a simple script that tells Repl.it that they
+ * should show you the content inside the ./public folder
+ * when you click "run".
+ */
+
 const express = require('express');
 
 const app = express();
